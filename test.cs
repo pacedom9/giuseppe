4a4b51ad6876f7565836aa9e876e12f91b0339e1
pacedom9 <pacedomenico73@gmail.com>
2024-11-04 12:05:57 +0100

    add comment

--- test.cs
+++ test.cs
@@ -1,5 +1,5 @@
 Console.WriteLine("ciao");
 
-Console.WriteLine("Questo potrebbe risultare in un conflitto");
+Console.WriteLine("Questo potrebbe risultare in un conflitto"); //COMMIT
 
 Console.WriteLine("ciao1");
